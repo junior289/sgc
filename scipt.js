a27faf59a00c4ade75be156bf5fef9be54920664
Manny <jsamba289@gmail.com>
2025-03-27 13:53:44 +0100

    Initial commit

---
+++ scipt.js
@@ -0,0 +1,6 @@
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Ajoute ici ton code pour gérer la soumission du formulaire
+    alert('Formulaire soumis!');
+  });
+  
